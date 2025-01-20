fa0e573ef827ea3d415d38db48186eac7f22c11d
imanee11 <imaneelouannane@gmail.com>
2025-01-20 14:32:47 +0100

    add and delete some files

---
+++ src/pages/home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import NavBar from '../components/home/NavBar';
+
+const Home = () => {
+    return (
+        <>
+            <NavBar/>
+        </>
+    );
+};
+
+export default Home;
