efe78e5849859ffe7aee1c8e3bd0e1ce39191db7
imanee11 <imaneelouannane@gmail.com>
2025-01-21 13:03:10 +0100

    test push error

--- src/components/home/Main.jsx
+++ src/components/home/Main.jsx
@@ -14,101 +14,7 @@ const Main = () => {
 
     const [activeTab, setActiveTab] = useState(1);
 
-    const tabs = [
-        {
-            id: 1,
-            title: "Sous traitance Comptable",
-            content: (
-                <div className='flex flex-col-reverse lg:flex-row gap-5'>
-                    <div className='lg:w-[50vw]'>
-                        <p className="pb-3 text-likeblack font-semibold text-[18px]">
-                            Sous traitance pour cabinets d'expertise comptable
-                        </p>
-                        <p>Nous proposons une gestion externalisée complète de votre comptabilité pour optimiser le fonctionnement de votre entreprise tout en garantissant la qualité et la fiabilité des informations comptables et financières. Nos services incluent :</p>
-                        <div className='pt-3'>
-                            <p className='flex items-center gap-3 pb-3'><FaCircleCheck className='text-accent' />Saisie et traitement des données comptables</p>
-                            <p className='flex items-center gap-3 pb-3'><FaCircleCheck className='text-accent' />Préparation des déclarations fiscales</p>
-                            <p className='flex items-center gap-3 pb-3'><FaCircleCheck className='text-accent' />Production des comptes annuels et/ou intermédiaires </p>
-                            <p className='flex items-center gap-3 pb-3'><FaCircleCheck className='text-accent' />Révision globale de la comptabilité</p>
-                            <p className='flex items-center gap-3 pb-3'><FaCircleCheck className='text-accent' />Rattrapage des comptabilités en retard ou mal gérées</p>
-                            {/* link */}
-                            <p className='flex items-center gap-3 underline cursor-pointer transition duration-300 text-accent'>Plus details<FaLongArrowAltRight className='text-accent' /></p>
-                        </div>
 
-                    </div>
-                    <div className='lg:w-[50vw]'>
-                        <img
-                            src={image.pic1}
-                            alt="Sous traitance Comptable"
-                            className="rounded-lg shadow-lg"
-                        />
-                    </div>
-                </div>
-            ),
-        },
-        {
-            id: 2,
-            title: "Sous traitance paie et social",
-            content: (
-                <div className='flex  flex-col-reverse lg:flex-row gap-5'>
-                    <div className='lg:w-[50vw]'>
-                        <p className="pb-3 text-likeblack font-semibold text-[18px]">
-                            Gestion externalisée efficace de la paie et des ressources humaines
-                        </p>
-                        <p>Nous offrons un service externalisé complet pour la gestion de la paie et des ressources humaines, vous aidant à maîtriser vos coûts et à améliorer votre organisation. Nos prestations incluent :</p>
-                        <div className='pt-3'>
-                            <p className='flex items-center gap-3 pb-3'><FaCircleCheck className='text-accent' />Traitement des données de paie</p>
-                            <p className='flex items-center gap-3 pb-3'><FaCircleCheck className='text-accent' />Gestion centralisée des données</p>
-                            <p className='flex items-center gap-3 pb-3'><FaCircleCheck className='text-accent' />Émission des fiches de paie</p>
-                            <p className='flex items-center gap-3 pb-3'><FaCircleCheck className='text-accent' />Préparation des obligations sociales</p>
-                            <p className='flex items-center gap-3 pb-3'><FaCircleCheck className='text-accent' />Gestion des droits et obligations des salariés</p>
-                            {/* link */}
-                            <p className='flex items-center gap-3 underline cursor-pointer transition duration-300 text-accent'>Plus details<FaLongArrowAltRight className='text-accent' /></p>
-                        </div>
-
-                    </div>
-                    <div className='lg:w-[50vw]'>
-                        <img
-                            src={image.pic2}
-                            alt="Sous traitance Comptable"
-                            className="rounded-lg shadow-lg"
-                        />
-                    </div>
-                </div>
-            ),
-        },
-        {
-            id: 3,
-            title: "Prestations Connexes",
-            content: (
-                <div className='flex  flex-col-reverse lg:flex-row gap-5'>
-                    <div className='lg:w-[50vw]'>
-                        <p className="pb-3 text-likeblack font-semibold text-[18px]">
-                            Gestion externalisée des outils d’aide à la prise de décisions
-                        </p>
-                        <p>Nous proposons la création de budgets, bilans prévisionnels et tableaux de bord pour fournir aux décideurs des indicateurs de gestion fiables et opportuns, essentiels aux décisions stratégiques.</p>
-                        <div className='pt-3'>
-                            <p className='flex items-center gap-3 pb-3'><FaCircleCheck className='text-accent' />Élaboration et suivi des budgets</p>
-                            <p className='flex items-center gap-3 pb-3'><FaCircleCheck className='text-accent' />Bilans prévisionnels</p>
-                            <p className='flex items-center gap-3 pb-3'><FaCircleCheck className='text-accent' />Tableaux de bord</p>
-                            {/* <p className='flex items-center gap-3 pb-3'><FaCircleCheck className='text-accent' />Préparation des obligations sociales</p> */}
-                            {/* <p className='flex items-center gap-3 pb-3'><FaCircleCheck className='text-accent' />Gestion des droits et obligations des salariés</p> */}
-                            {/* link */}
-                            <p className='flex items-center gap-3 underline cursor-pointer transition duration-300 text-accent'>Plus details<FaLongArrowAltRight className='text-accent' /></p>
-                        </div>
-
-                    </div>
-                    <div className='lg:w-[50vw]'>
-                        <img
-                            src={image.pic3}
-                            alt="Sous traitance Comptable"
-                            className="rounded-lg shadow-lg"
-                        />
-                    </div>
-                </div>
-            ),
-        },
-    ];
 
 
     return (
@@ -182,7 +88,34 @@ const Main = () => {
 
 
                         {/* services */}
+                        <section id='services' className="lg:py-[15vh] pt-[7vh] font-baloo">
+                            <p className=" text-likeblack font-sans  text-center text-[19px] uppercase font-bold  ">Nos services</p>
+                            <div className="w-full mx-auto lg:pt-8 pt-4">
+                                {/* Tab Buttons */}
+                                <div className="flex flex-wrap gap-[2vw] justify-center border-b border-gray-300">
+                                    {tabs.map((tab) => (
+                                        <button
+                                            key={tab.id}
+                                            onClick={() => setActiveTab(tab.id)}
+                                            className={`px-6 py-2 text-[15px] font-semibold transition duration-300 ${activeTab === tab.id
+                                                ? "text-primary border-b-2 border-primary"
+                                                : "text-gray-500 hover:text-primary"
+                                                }`}
+                                        >
+                                            {tab.title}
+                                        </button>
+                                    ))}
+                                </div>
 
+                                {/* Tab Content */}
+                                <div className=" pt-[5vh]">
+                                    {tabs.find((tab) => tab.id === activeTab)?.content}
+                                </div>
+                            </div>
+
+
+
+                        </section>
                     </section>
                 </section>
 
