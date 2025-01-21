3b9cbebf9722d6bf8fe2a9af047ea1ba140ffcbf
imanee11 <imaneelouannane@gmail.com>
2025-01-21 15:33:58 +0100

    add react slick

--- src/components/home/Main.jsx
+++ src/components/home/Main.jsx
@@ -6,6 +6,10 @@ import { FaCircleCheck } from "react-icons/fa6";
 import { FaLongArrowAltRight } from "react-icons/fa";
 
 
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+
 
 import image from '../../constants/image';
 
