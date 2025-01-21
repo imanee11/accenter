5bdd426720e08f5769ff4fdc0849453e2d57b883
imanee11 <imaneelouannane@gmail.com>
2025-01-21 16:45:39 +0100

    add second card of the carousel

--- src/components/home/Carousel.jsx
+++ src/components/home/Carousel.jsx
@@ -9,12 +9,16 @@ import { RxDotFilled } from "react-icons/rx";
 function Carousel() {
     var settings = {
         dots: true,
-        infinite: false,
+        infinite: true,
         speed: 500,
         slidesToShow: 3,
         slidesToScroll: 3,
         initialSlide: 0,
-        
+        slidesToScroll: 1,
+        // autoplay: true,
+        // autoplaySpeed: 2000,
+        // pauseOnHover: true,
+
         responsive: [
             {
                 breakpoint: 1024,
@@ -45,23 +49,29 @@ function Carousel() {
     return (
         <div className="slider-container">
             <Slider {...settings}>
-                <div>
+                <div className="p-2 ">
                     <div className="flex flex-col  rounded-xl  border-[1px] border-gray-300 bg-white  cursor-pointer hover:shadow-[0px_0px_0px_1px_rgba(0,0,0,0.06),0px_1px_1px_-0.5px_rgba(0,0,0,0.06),0px_3px_3px_-1.5px_rgba(0,0,0,0.06),_0px_6px_6px_-3px_rgba(0,0,0,0.06),0px_12px_12px_-6px_rgba(0,0,0,0.06),0px_24px_24px_-12px_rgba(0,0,0,0.06)]  transition-transform duration-300 ">
                         <div className="">
                             <img src={image.pic4} alt="" className="rounded-t-xl" />
                         </div>
                         <div className="p-4">
                             <p className="text-lg font-semibold">Réduire vos coûts</p>
-                            <p className="mt-2 text-likeblack">Grâce à nos tarifs compétitifs et notre expertise, AC Center réduit vos dépenses liées à la gestion comptable et sociale. Cela libère vos équipes pour des tâches à forte valeur ajoutée et vous évite :</p>
-                            <p className="flex items-center gap-3 pt-3 text-likeblack"><RxDotFilled />Investissements informatiques inutiles.</p>
-                            <p className="flex items-center gap-3 pt-2 text-likeblack"><RxDotFilled />Paramétrage et maintenance de logiciels.</p>
+                            <p className="mt-2 text-likeblack">Grâce à nos tarifs compétitifs et notre expertise, AC Center optimise vos processus comptables et sociaux, libérant ainsi vos équipes des tâches administratives. Profitez d’économies sur l’investissement informatique, le paramétrage et la maintenance des logiciels.</p>
                         </div>
 
                     </div>
 
                 </div>
-                <div>
-                    <h3>2</h3>
+                <div className="p-2">
+                    <div className="flex flex-col  rounded-xl  border-[1px] border-gray-300 bg-white  cursor-pointer hover:shadow-[0px_0px_0px_1px_rgba(0,0,0,0.06),0px_1px_1px_-0.5px_rgba(0,0,0,0.06),0px_3px_3px_-1.5px_rgba(0,0,0,0.06),_0px_6px_6px_-3px_rgba(0,0,0,0.06),0px_12px_12px_-6px_rgba(0,0,0,0.06),0px_24px_24px_-12px_rgba(0,0,0,0.06)]  transition-transform duration-300 ">
+                        <div className="">
+                            <img src={image.pic4} alt="" className="rounded-t-xl" />
+                        </div>
+                        <div className="p-4">
+                            <p className="text-lg font-semibold">Assurer la qualité de vos services</p>
+                            <p className="mt-2 text-likeblack">AC Center met à votre disposition une équipe qualifiée, dirigée par des experts en comptabilité, gestion sociale, management et informatique, et encadrée par un expert-comptable inscrit à l'Ordre des Experts-Comptables, garantissant ainsi la qualité de nos services.</p>
+                        </div>
+                    </div>
                 </div>
                 <div>
                     <h3>3</h3>
